ac6eb3ab404c1be7b197c7428f2c7903ec75d51e
Tên của bạn <email@example.com>
2025-03-27 18:04:58 +0700

    init

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
-    return "helloworld"
+    return "<h2> helloworld </h2>"
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'Long':
+        return redirect(url_for('admin'))
+    return f"<h1> hello {name}! </h1>"
+
+
+@app.route('/blog/<int:blog_id>')
+def blog(blog_id):
+    return f"<h1> blog {blog_id} </h1>"
+
+@app.route('/admin')
+def admin():
+    return "<h1> Hello, admin! "
 
 
 if __name__ == "__main__":
