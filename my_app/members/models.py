61bb813ad6fc3376427242847d920a00417b89bb
Tên của bạn <email@example.com>
2025-03-30 00:06:39 +0700

    Admin

--- my_app/members/models.py
+++ my_app/members/models.py
@@ -4,5 +4,6 @@ class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
+  email = models.EmailField(null=True)
   joined_date = models.DateField(null=True)
   
--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qov8t+uc4+ch@_48gwtg8s^vy%dk#6z*f^t0+)+*wgdn__1ous'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
